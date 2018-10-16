366e3cdf26c936caa5d4ed0ac61c93ab115f44fe
gibeulah <gibeulah@microsoft.com>
2018-10-17 01:15:10 +0530

    First Commit

---
+++ MatrixScalarMultiplication/MatrixManipulator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixScalarMultiplication
+{
+    class MatrixManipulator
+    {
+        int[,] matrix;
+
+        private MatrixManipulator() { }
+
+        public MatrixManipulator(int[,] matrix) {
+            this.matrix = matrix;
+        }
+
+        public void PrintScalarMultipliedMatrix(int scalar) {
+
+            for (int i = 0; i < this.matrix.GetLength(0); i++) {
+                for (int j = 0; j < this.matrix.GetLength(1); j++) {
+                    this.matrix[i, j] = scalar * this.matrix[i, j];
+                }
+            }
+
+            PrintMatrix();
+        }
+
+        public void PrintMatrix(){
+            for (int i = 0; i < this.matrix.GetLength(0); i++) {
+                for (int j = 0; j < this.matrix.GetLength(1); j++) {
+                    Console.Write(matrix[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- MatrixScalarMultiplication/Program.cs
+++ MatrixScalarMultiplication/Program.cs
@@ -6,7 +6,47 @@ namespace MatrixScalarMultiplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Matrix Scalar Multiplication!");
+            Console.WriteLine("------------------------------");
+            try
+            {
+                int input = 1;
+                do
+                {
+                    Console.WriteLine("Enter the number of rows");
+                    int rows = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the number of columns");
+                    int cols = int.Parse(Console.ReadLine());
+                    int[,] matrix = new int[rows, cols];
+                    Console.WriteLine("Enter the contents of the matrix");
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            matrix[i, j] = int.Parse(Console.ReadLine());
+                        }
+                    }
+
+                    Console.WriteLine("Enter the scalar for multiplication");
+                    int scalar = int.Parse(Console.ReadLine());
+                    MatrixManipulator matrixManipulator = new MatrixManipulator(matrix);
+                    Console.WriteLine("Original Matrix-----------------------");
+                    matrixManipulator.PrintMatrix();
+                    Console.WriteLine();
+                    Console.WriteLine("Scalar Multiplied Matrix-----------------------");
+                    matrixManipulator.PrintScalarMultipliedMatrix(scalar);
+                    Console.WriteLine();
+
+
+                    Console.WriteLine("Enter 1 to continue and 0 to stop");
+                    input = int.Parse(Console.ReadLine());
+                } while (input == 1);
+            }
+            catch (Exception exception) {
+                Console.WriteLine("The thrown exception is"+exception.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
