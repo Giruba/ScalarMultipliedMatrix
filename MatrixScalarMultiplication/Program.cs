be85c4aef6e22c1973f9a5e4c9d988de8d13e494
gibeulah <gibeulah@microsoft.com>
2018-10-17 00:15:50 +0530

    Add project files.

---
+++ MatrixScalarMultiplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixScalarMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
